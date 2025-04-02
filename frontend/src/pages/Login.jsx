0afa71e7874411cd206e8bacef3dae7b1f29e7de
Syed Misbah <85030882+smisbah38@users.noreply.github.com>
2025-04-03 03:05:38 +0600

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -57,7 +57,7 @@ const Login = () => {
   return (
     <form
       onSubmit={onSubmitHandler}
-      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-40 gap-4 text-gray-300"
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-10 gap-4 text-gray-300"
     >
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
